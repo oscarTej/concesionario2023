3bbdeb757dedf527f805a9a2e6b98f2ee24c210a
oscarTej <146753497+oscarTej@users.noreply.github.com>
2023-11-30 12:08:37 +0100

    comenzamos el proyecto. estructura de clases

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
-    public static void main(String[] args) {
+
+public static void main(String[] args) {
+
+    // proceso de alta
+    Vehiculo Vehiculo  = new Vehiculo();
+    vehiculo.setMatricula("01233x");
+
     }
+
 }
-
-
